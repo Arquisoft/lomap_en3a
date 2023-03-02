26bfd29ac5d0667a43e29748ca2b8d558eb29d99
David Martínez Castañón <UO282048@uniovi.es>
2023-03-02 22:25:28 +0100

    feat: update app title and favicon

--- webapp/src/index.tsx
+++ webapp/src/index.tsx
@@ -7,6 +7,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+document.title = "LoMap";
+
 root.render(
   <React.StrictMode>
     <App />
