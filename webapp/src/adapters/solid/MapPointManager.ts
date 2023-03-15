d365f8a625695461b96e596252550208b220c798
Pelayo-Reguera <UO282348@uniovi.es>
2023-03-15 18:10:56 +0100

    Methods to add a new MapPoint in the pod of the user

--- webapp/src/adapters/solid/MapPointManager.ts
+++ webapp/src/adapters/solid/MapPointManager.ts
@@ -1,12 +1,10 @@
 import {
     buildThing,
     createSolidDataset,
-    createThing, getInteger,
+    createThing,
     getPodUrlAll, getSolidDataset,
-    getStringNoLocale, getThingAll,
     saveSolidDatasetAt,
     setThing,
-    Thing
 } from "@inrupt/solid-client";
 import {SCHEMA_INRUPT} from "@inrupt/vocab-common-rdf";
 import SolidSessionManager from "./SolidSessionManager";
@@ -22,10 +20,17 @@ export default class MapPointManager {
      * */
     public async createNewMapPoint(newLocation: MapPoint): Promise<void> {
         const podURL = await getPodUrlAll(this.sessionManager.getWebID(), {fetch: fetch});
-        this.createNewDataSet(podURL[0] + "public/" + this.MAPPOINTSURL, newLocation);
+        try{
+            await this.appendToDataSet(podURL[0] + "public/" + this.MAPPOINTSURL, newLocation);
+        } catch(fe){
+            await this.createNewDataSet(podURL[0] + "public/" + this.MAPPOINTSURL, newLocation);
+        }
     }
 
-    private async createNewDataSet(podURL: string, newLocation: MapPoint) : Promise<null>{
+    /**
+     * It creates a new DataSet file and adds the map point
+     * */
+    private async createNewDataSet(podURL: string, newLocation: MapPoint): Promise<null> {
         let courseSolidDataset = createSolidDataset();
         let placemark = buildThing(createThing({name: crypto.randomUUID()}))
             .addStringNoLocale(SCHEMA_INRUPT.name, newLocation.getTitle())
@@ -33,24 +38,8 @@ export default class MapPointManager {
             .addInteger(SCHEMA_INRUPT.longitude, newLocation.getLng())
             .build();
         courseSolidDataset = setThing(courseSolidDataset, placemark);
-        const savedSolidDataset = await saveSolidDatasetAt(
-            podURL,
-            courseSolidDataset,
-            {fetch: this.sessionManager.getSessionFetch()}// fetch from authenticated Session
-        );
-        return null;
-    }
-
-    private async appendToDataSet(podURL: string, newLocation: MapPoint): Promise<null>{
-        let courseSolidDataset = await getSolidDataset(podURL, { fetch: this.sessionManager.getSessionFetch()});
-        let placemark = buildThing(createThing({name: crypto.randomUUID()}))
-            .addStringNoLocale(SCHEMA_INRUPT.name, newLocation.getTitle())
-            .addInteger(SCHEMA_INRUPT.latitude, newLocation.getLat())
-            .addInteger(SCHEMA_INRUPT.longitude, newLocation.getLng())
-            .build();
-        courseSolidDataset = setThing(courseSolidDataset, placemark);
-        const savedSolidDataset = await saveSolidDatasetAt(
-            podURL,
+        await saveSolidDatasetAt(
+            podURL[0],
             courseSolidDataset,
             {fetch: this.sessionManager.getSessionFetch()}// fetch from authenticated Session
         );
@@ -58,22 +47,21 @@ export default class MapPointManager {
     }
 
     /**
-     * From a webID it returns all the information from a person in a User object
-     *
-     * @param webID A string identifying the pod of a user
-     *
-     * @return The object User corresponding to the POD of the webID
+     * This method append the information to an existing file in the POD's user
      * */
-    public async getMapPointData(webID: string): Promise<MapPoint> {
-        const podURL = await getPodUrlAll(this.sessionManager.getWebID(), {fetch: fetch});
-        let webId = podURL + "public/MapPoints";
-        let webIdDoc = await getSolidDataset(webId, {fetch: this.sessionManager.getSession().fetch});
-        let friends = getThingAll(webIdDoc);
-        //It returns all the values in the knows property of the object Thing
-        let name = getStringNoLocale(<Thing>friends[0], SCHEMA_INRUPT.name);
-        let lat = getInteger(<Thing>friends[0], SCHEMA_INRUPT.latitude);
-        let long = getInteger(<Thing>friends[0], SCHEMA_INRUPT.longitude);
-        // return new Placemark(name, webID);
-        return new Placemark(<number>lat, <number>long, <string>name);
+    private async appendToDataSet(podURL: string, newLocation: MapPoint): Promise<null> {
+        let courseSolidDataset = await getSolidDataset(podURL[0], {fetch: this.sessionManager.getSessionFetch()});
+        let placemark = buildThing(createThing({name: crypto.randomUUID()}))
+            .addStringNoLocale(SCHEMA_INRUPT.name, newLocation.getTitle())
+            .addInteger(SCHEMA_INRUPT.latitude, newLocation.getLat())
+            .addInteger(SCHEMA_INRUPT.longitude, newLocation.getLng())
+            .build();
+        courseSolidDataset = setThing(courseSolidDataset, placemark);
+        await saveSolidDatasetAt(
+            podURL,
+            courseSolidDataset,
+            {fetch: this.sessionManager.getSessionFetch()}// fetch from authenticated Session
+        );
+        return null;
     }
 }
