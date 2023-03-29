ece9d9851af730c3d64d56af0b2b17244f41dfa2
David Martínez Castañón <UO282048@uniovi.es>
2023-03-29 18:44:07 +0200

    feat: add an assembler for creating things from objects

---
+++ webapp/src/adapters/solid/Assembler.ts
@@ -0,0 +1,9 @@
+import { buildThing, createSolidDataset, createThing, setThing, SolidDataset, Thing } from '@inrupt/solid-client';
+import { SCHEMA_INRUPT } from '@inrupt/vocab-common-rdf';
+import Map from '../../domain/Map';
+import Placemark from '../../domain/Placemark';
+
+export default class Assembler {
+
+
+}
