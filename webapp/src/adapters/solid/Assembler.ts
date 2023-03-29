c563bda875b4d210f981868b9fbae43c83b1e656
David Martínez Castañón <UO282048@uniovi.es>
2023-03-29 18:44:50 +0200

    feat: add a method for creating datasets from maps

--- webapp/src/adapters/solid/Assembler.ts
+++ webapp/src/adapters/solid/Assembler.ts
@@ -5,5 +5,27 @@ import Placemark from '../../domain/Placemark';
 
 export default class Assembler {
 
+    public static toDataset(map: Map): SolidDataset {
+        let dataset = createSolidDataset();
+        let details =  buildThing(createThing({name: "Details"}))
+            .addStringNoLocale(SCHEMA_INRUPT.name, map.getName())
+            .addStringNoLocale(SCHEMA_INRUPT.description, map.getDescription())
+            .build();
+
+        dataset = setThing(dataset, details);
+
+        for (let p of map.getPlacemarks()) {
+            dataset = setThing(dataset, this.toThing(p));
+        }
+        return dataset;
+    }
+
+    public static toThing(placemark: Placemark): Thing {
+        return  buildThing(createThing())
+            .addStringNoLocale(SCHEMA_INRUPT.name, placemark.getTitle())
+            .addDecimal(SCHEMA_INRUPT.latitude, placemark.getLat())
+            .addDecimal(SCHEMA_INRUPT.longitude, placemark.getLng())
+            .build();
+    }
 
 }
