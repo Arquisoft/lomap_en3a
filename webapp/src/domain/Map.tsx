741628667dd624ec0e9ce7effadf11b19325f815
David Martínez Castañón <UO282048@uniovi.es>
2023-04-06 11:43:40 +0200

    feat: add an ID field to the maps

--- webapp/src/domain/Map.tsx
+++ webapp/src/domain/Map.tsx
@@ -2,17 +2,22 @@ import Placemark from "./Placemark";
 
 export default class Map {
     private placemarks: Array<Placemark> = new Array<Placemark>();
-    private id: string = crypto.randomUUID();
+    private id: string;
     private name: string;
     private description: string;
 
-    public constructor(name:string, description:string="") {
+    public constructor(name:string, description:string="", id:string|null=null) {
         this.name = name;
         this.description = description;
+        this.id = (id === null) ? crypto.randomUUID() : id;
     }
 
     public add(p: Placemark): void {
         this.placemarks.push(p);
+    }
+
+    public setPlacemarks(p: Placemark[]): void {
+        this.placemarks = p;
     }
 
     public remove(p: Placemark): void {
