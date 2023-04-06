25c3d8b09110492f01e0e391b2d77eaa395ef1dd
David Martínez Castañón <UO282048@uniovi.es>
2023-04-06 11:49:30 +0200

    feat: create maps from SPARQL query bindings

--- webapp/src/adapters/solid/Assembler.ts
+++ webapp/src/adapters/solid/Assembler.ts
@@ -1,14 +1,17 @@
 import { buildThing, createSolidDataset, createThing, setThing, SolidDataset, Thing } from '@inrupt/solid-client';
-import { SCHEMA_INRUPT } from '@inrupt/vocab-common-rdf';
+import { SCHEMA_INRUPT, RDF } from '@inrupt/vocab-common-rdf';
 import Map from '../../domain/Map';
 import Placemark from '../../domain/Placemark';
+import { Bindings } from 'rdf-js';
 
 export default class Assembler {
 
     public static toDataset(map: Map): SolidDataset {
         let dataset = createSolidDataset();
-        let details =  buildThing(createThing({name: "Details"}))
+        let details =  buildThing(createThing({name: "details"}))
+            .addStringNoLocale(RDF.type, 'details')
             .addStringNoLocale(SCHEMA_INRUPT.name, map.getName())
+            .addStringNoLocale(SCHEMA_INRUPT.identifier, map.getId())
             .addStringNoLocale(SCHEMA_INRUPT.description, map.getDescription())
             .build();
 
@@ -22,10 +25,30 @@ export default class Assembler {
 
     public static toThing(placemark: Placemark): Thing {
         return  buildThing(createThing())
+            .addStringNoLocale(RDF.type, 'placemark')
             .addStringNoLocale(SCHEMA_INRUPT.name, placemark.getTitle())
             .addDecimal(SCHEMA_INRUPT.latitude, placemark.getLat())
             .addDecimal(SCHEMA_INRUPT.longitude, placemark.getLng())
             .build();
+    }
+
+    public static toMapPreviews(bindings: Bindings[]): Array<Map> {
+        let result: Array<Map> = new Array();
+
+        for (let binding of bindings) {
+            this.addMapPreview(binding, result);
+        }
+        return result;
+    }
+
+    private static addMapPreview(binding: Bindings, maps: Array<Map>): void {
+        let id = binding.get("id")?.value;
+        let name = binding.get("name")?.value;
+        let desc = binding.get("desc")?.value;
+
+        if (id!==undefined && name!==undefined && desc!==undefined) {
+            maps.push(new Map(name, desc, id));
+        }
     }
 
 }
