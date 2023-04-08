3b177cf678f54066f194161a35415781c739f45a
Guillermo Dylan Carvajal Aza <98907563+UO283069@users.noreply.github.com>
2023-04-08 17:52:51 +0200

    First steps with the unitary tests, an error has been found when trying to run this tests, as a leaflet module seems to have problems being compiled by jest.

---
+++ webapp/src/components/place/ReviewsPage.test.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {fireEvent, render} from "@testing-library/react";
+import PointInformation from '../../pages/PointInformation';
+import Place from '../../domain/Place';
+import Map from "../../domain/Map";
+
+test('check that the list of users renders propertly', async () => {
+    const point: Place = new Place("Test", 100, 100, "Test", new Array());
+    const map: Map = new Map();
+    const {getByText} = render(<PointInformation point={point} map={map}></PointInformation>);
+    // We click the Reviews button
+    const button = getByText("Reviews");
+    fireEvent.click(button);
+    // We should see the comments and Average Rating
+    expect(getByText("Comments")).toBeInTheDocument();
+    expect(getByText("AverageRating")).toBeInTheDocument();
+});
