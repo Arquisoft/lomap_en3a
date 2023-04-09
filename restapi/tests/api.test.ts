a525f61be018905ef4bd982a467f7a7203ccb0c4
Iván Menéndez Mosegui <98897888+uo282892@users.noreply.github.com>
2023-04-09 20:25:42 +0200

    Initial configuration of restapi tests, getPlaces and valid addPlace tests implemented

--- restapi/tests/api.test.ts
+++ restapi/tests/api.test.ts
@@ -4,6 +4,9 @@ import * as http from 'http';
 import bp from 'body-parser';
 import cors from 'cors';
 import api from '../api';
+import {PlaceType} from "../src/types/PlaceType";
+import Place from "../src/models/Place";
+import * as mongoose from "mongoose";
 
 let app:Application;
 let server:http.Server;
@@ -23,28 +26,52 @@ beforeAll(async () => {
     }).on("error",(error:Error)=>{
         console.error('Error occured: ' + error.message);
     });
+
+    await mongoose.connect("mongodb+srv://lomapen3a:HkO74fQ5tNY8gvfj@lomapen3a.sscscjd.mongodb.net/lomapen3a?retryWrites=true&w=majority");
 });
 
 afterAll(async () => {
     server.close() //close the server
+    await mongoose.disconnect();
 })
 
-describe('user ', () => {
+describe('place ', () => {
+
+    jest.setTimeout(8000);
+
     /**
-     * Test that we can list users without any error.
+     * Listing places without errors
      */
-    it('can be listed',async () => {
-        const response:Response = await request(app).get("/api/users/list");
+    it('listing places',async () => {
+        const response:Response = await request(app).get("/api/places/list");
         expect(response.statusCode).toBe(200);
+        expect(response.body.length).toBeGreaterThan(0);
+        expect(response.body[0].title.length).toBeGreaterThan(0);
+        expect(response.body[0].uuid.length).toBeGreaterThan(0);
+        expect(response.body[0].longitude).toBeGreaterThan(-180);
+        expect(response.body[0].longitude).toBeLessThan(180);
+        expect(response.body[0].latitude).toBeGreaterThan(-90);
+        expect(response.body[0].latitude).toBeLessThan(90);
     });
 
     /**
-     * Tests that a user can be created through the productService without throwing any errors.
+     * Correct creation of a user
      */
     it('can be created correctly', async () => {
-        let username:string = 'Pablo'
-        let email:string = 'gonzalezgpablo@uniovi.es'
-        const response:Response = await request(app).post('/api/users/add').send({name: username,email: email}).set('Accept', 'application/json')
+        let title:string = 'TestPlace'
+        let uuid:string = 'https://lomapen3a.inrupt.net/profile/card#me'
+        let longitude:number = 4.56
+        let latitude:number = 7.35
+        const response:Response = await request(app).post('/api/places/add')
+            .send({title: title, uuid: uuid, longitude: longitude, latitude: latitude}).set('Accept', 'application/json')
         expect(response.statusCode).toBe(200);
+
+        let place: PlaceType = await Place.findOne({title: title}) as PlaceType;
+        expect(place.title).toBe(title);
+        expect(place.uuid).toBe(uuid);
+        expect(place.longitude).toBe(longitude);
+        expect(place.latitude).toBe(latitude);
+
+        await Place.deleteOne({title: title});
     });
 });
