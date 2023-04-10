eee3bc03e8ba0cf4f3a2e0f062405ad5aa067957
Iván Menéndez Mosegui <98897888+uo282892@users.noreply.github.com>
2023-04-10 15:42:40 +0200

    Implemented remaining validation tests
    Extra: Fixed lower and upper limits for latitude in validation (-90, 90)

--- restapi/tests/api.test.ts
+++ restapi/tests/api.test.ts
@@ -37,7 +37,7 @@ afterAll(async () => {
 
 describe('place ', () => {
 
-    jest.setTimeout(15000);
+    jest.setTimeout(12000);
 
     /**
      * Listing places without errors
@@ -55,7 +55,7 @@ describe('place ', () => {
     });
 
     /**
-     * Correct creation of a user
+     * Correct creation of a place
      */
     it('can be created correctly', async () => {
         let title:string = 'TestPlace'
@@ -77,7 +77,7 @@ describe('place ', () => {
     });
 
     /**
-     * Incorrect creation of a user (empty title)
+     * Incorrect creation of a place (empty title)
      */
     it('cannot be created correctly (empty title)', async () => {
         let title:string = ''
@@ -86,11 +86,60 @@ describe('place ', () => {
         let latitude:number = 7.35
         const response:Response = await request(app).post('/api/places/add')
             .send({title: title, uuid: uuid, longitude: longitude, latitude: latitude}).set('Accept', 'application/json')
+        //After trying to add it, we should get an error telling about an invalid value for title
         expect(response.statusCode).toBe(400);
+        expect(response.body).toHaveProperty('errors[0].param', "title")
+        expect(response.body).toHaveProperty('errors[0].msg', "Title must be provided and it cannot be empty")
     });
 
     /**
-     * Correct deletion of a user
+     * Incorrect creation of a place (empty uuid)
+     */
+    it('cannot be created correctly (empty uuid)', async () => {
+        let title:string = 'IncorrectPlace'
+        let uuid:string = ''
+        let longitude:number = 18.22
+        let latitude:number = 25.98
+        const response:Response = await request(app).post('/api/places/add')
+            .send({title: title, uuid: uuid, longitude: longitude, latitude: latitude}).set('Accept', 'application/json')
+        //After trying to add it, we should get an error telling about an invalid value for uuid
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toHaveProperty('errors[0].param', "uuid")
+        expect(response.body).toHaveProperty('errors[0].msg', "Pod uuid must be provided and it cannot be empty")
+    });
+
+    /**
+     * Incorrect creation of a place (invalid coordinates, outside both lower and upper limits)
+     */
+    it('cannot be created correctly (invalid coordinates)', async () => {
+        let title:string = 'IncorrectPlace'
+        let uuid:string = 'uuid1'
+        let longitude:number = -181
+        let latitude:number = -91
+        let response:Response = await request(app).post('/api/places/add')
+            .send({title: title, uuid: uuid, longitude: longitude, latitude: latitude}).set('Accept', 'application/json')
+        /* After trying to add it, we should get two errors telling about invalid values, one for latitude, the other
+        for longitude*/
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toHaveProperty('errors[0].param', "longitude")
+        expect(response.body).toHaveProperty('errors[0].msg', "Longitude must be between -180 and 180")
+        expect(response.body).toHaveProperty('errors[1].param', "latitude")
+        expect(response.body).toHaveProperty('errors[1].msg', "Latitude must be between -90 and 90")
+
+        longitude = 181
+        latitude = 91
+        response = await request(app).post('/api/places/add')
+            .send({title: title, uuid: uuid, longitude: longitude, latitude: latitude}).set('Accept', 'application/json')
+        //Both coordinates still have invalid values, we should get the same errors
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toHaveProperty('errors[0].param', "longitude")
+        expect(response.body).toHaveProperty('errors[0].msg', "Longitude must be between -180 and 180")
+        expect(response.body).toHaveProperty('errors[1].param', "latitude")
+        expect(response.body).toHaveProperty('errors[1].msg', "Latitude must be between -90 and 90")
+    });
+
+    /**
+     * Correct deletion of a place
      */
     it('can be deleted correctly', async () => {
         let title:string = 'TestPlace'
@@ -120,7 +169,7 @@ describe('place ', () => {
         let uuid:string = 'originalUuid'
         let longitude:number = 1
         let latitude:number = 1
-        // We demonstrate that, at this point, we have the original values
+        // We demonstrate that, at this point, the place has the original values
         let place: PlaceType = await Place.create({title: title, uuid: uuid, longitude: longitude, latitude: latitude}) as PlaceType
         expect(place.title).toBe(title);
         expect(place.uuid).toBe(uuid);
@@ -143,5 +192,39 @@ describe('place ', () => {
         expect(updatedPlace.latitude).toBe(updatedLatitude);
 
         await Place.deleteOne({title: updatedTitle});
+    });
+
+
+    /**
+     * Incorrect update of a place (invalid values)
+     */
+    it('cannot be updated correctly (invalid values)', async () => {
+        let title:string = ''
+        let uuid:string = ''
+        let longitude:number = -181
+        let latitude:number = -91
+        let response:Response = await request(app).post('/api/places/update/Brussels town hall')
+            .send({title: title, uuid: uuid, longitude: longitude, latitude: latitude}).set('Accept', 'application/json')
+        //After trying to update it, we should get an error for each incorrect field
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toHaveProperty('errors[0].param', "title")
+        expect(response.body).toHaveProperty('errors[0].msg', "Title cannot be empty")
+        expect(response.body).toHaveProperty('errors[1].param', "uuid")
+        expect(response.body).toHaveProperty('errors[1].msg', "Pod uuid cannot be empty")
+        expect(response.body).toHaveProperty('errors[2].param', "longitude")
+        expect(response.body).toHaveProperty('errors[2].msg', "Longitude must be between -180 and 180")
+        expect(response.body).toHaveProperty('errors[3].param', "latitude")
+        expect(response.body).toHaveProperty('errors[3].msg', "Latitude must be between -90 and 90")
+
+        longitude = 181
+        latitude = 91
+        response = await request(app).post('/api/places/add')
+            .send({title: title, uuid: uuid, longitude: longitude, latitude: latitude}).set('Accept', 'application/json')
+        //Both coordinates still have invalid values, we should get the same errors for them
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toHaveProperty('errors[2].param', "longitude")
+        expect(response.body).toHaveProperty('errors[2].msg', "Longitude must be between -180 and 180")
+        expect(response.body).toHaveProperty('errors[3].param', "latitude")
+        expect(response.body).toHaveProperty('errors[3].msg', "Latitude must be between -90 and 90")
     });
 });
