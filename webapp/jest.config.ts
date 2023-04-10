131267be150f2a4e8e704ed9ed1b8b0a94956cc9
Guillermo Dylan Carvajal Aza <98907563+UO283069@users.noreply.github.com>
2023-04-10 16:20:45 +0200

    Fix for the tests with jest.

--- webapp/jest.config.ts
+++ webapp/jest.config.ts
@@ -1,7 +1,5 @@
-const esModules = ['@react-leaflet', 'react-leaflet'].join('|');
 export default {
-    transformIgnorePatterns: [`/node_modules/(?!${esModules})`],
     transform: {
         "^.+\\.tsx?$": "ts-jest"
-    },
+    }
 }
--- webapp/src/setupTests.ts
+++ webapp/src/setupTests.ts
@@ -3,3 +3,7 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import {TextDecoder, TextEncoder} from "util";
+
+global.TextEncoder = TextEncoder;
+(global as any).TextDecoder = TextDecoder;
