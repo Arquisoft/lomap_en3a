dbd812dbc59a789a8efe325697160b9a89137b77
Iván Menéndez Mosegui <98897888+uo282892@users.noreply.github.com>
2023-04-10 23:37:49 +0200

    Configured and implemented navigation menu buttons tests

--- webapp/src/components/NavMenu.test.tsx
+++ webapp/src/components/NavMenu.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import {fireEvent, getByText, render, screen} from '@testing-library/react';
 import NavMenu from "./NavMenu";
 import {BrowserRouter} from "react-router-dom";
 
@@ -11,6 +11,35 @@ test('renders nav menu', () => {
     expect(myPlacesOptionText).toBeInTheDocument();
     const friendsOptionText = screen.getByText("Friends");
     expect(friendsOptionText).toBeInTheDocument();
-    const publicMapOptionText = screen.getByText("Friends");
+    const publicMapOptionText = screen.getByText("Public map");
     expect(publicMapOptionText).toBeInTheDocument();
+});
+
+
+test('choose home option', () => {
+    const {container} = render(<BrowserRouter><NavMenu /></BrowserRouter>);
+    const homeOption = getByText(container,"Home");
+    fireEvent.click(homeOption);
+    expect(window.location.pathname).toBe("/home")
+});
+
+test('choose my places option', () => {
+    const {container} = render(<BrowserRouter><NavMenu /></BrowserRouter>);
+    const homeOption = getByText(container,"My places");
+    fireEvent.click(homeOption);
+    expect(window.location.pathname).toBe("/map/personal")
+});
+
+test('choose friends option', () => {
+    const {container} = render(<BrowserRouter><NavMenu /></BrowserRouter>);
+    const homeOption = getByText(container,"Friends");
+    fireEvent.click(homeOption);
+    expect(window.location.pathname).toBe("/friends")
+});
+
+test('choose friends option', () => {
+    const {container} = render(<BrowserRouter><NavMenu /></BrowserRouter>);
+    const homeOption = getByText(container,"Public map");
+    fireEvent.click(homeOption);
+    expect(window.location.pathname).toBe("/map/public")
 });
