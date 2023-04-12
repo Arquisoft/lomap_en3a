3ce05f9f700f6232ecb296f9cdb0db069806adad
David Martínez Castañón <UO282048@uniovi.es>
2023-04-12 19:25:28 +0200

    feat: add callback prop

--- webapp/src/components/tests/MapFilter.test.tsx
+++ webapp/src/components/tests/MapFilter.test.tsx
@@ -5,7 +5,7 @@ import {PlaceCategory} from '../../domain/Place/PlaceCategory';
 
 test('When the MapFilter renders, all the categories are shown', () => {
     const categories: string[] = Object.keys(PlaceCategory);
-    const {getByText} = render(<MapFilter></MapFilter>);
+    const {getByText} = render(<MapFilter callback={()=>{}}/>);
     for (const i in categories) {
         expect(getByText(categories[i])).toBeInTheDocument();
     }
