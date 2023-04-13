2f23d03e3b99ab42939ef9dea399ed8a86a92027
Guillermo Dylan Carvajal Aza <98907563+UO283069@users.noreply.github.com>
2023-04-13 16:02:58 +0200

    First steps on the redesign of the UI: start of the welcome menu.

--- webapp/src/pages/Home.tsx
+++ webapp/src/pages/Home.tsx
@@ -1,28 +1,27 @@
-import React from "react";
+import React, {ChangeEvent} from "react";
 import LeafletMapAdapter from "../adapters/map/LeafletMapAdapter";
 import SolidSessionManager from "../adapters/solid/SolidSessionManager";
 import MapFilter from "../components/MapFilter";
 import Map from "../domain/Map";
 import PODManager from "../adapters/solid/PODManager";
-import { ChangeEvent } from "react";
 import Placemark from "../domain/Placemark";
 import {PlaceType} from "../types/PlaceType";
 
 
 interface HomeProps {
-    placeList? : PlaceType[]
+    placeList?: PlaceType[]
 }
 
-export default class Home extends React.Component<HomeProps,{data:Map|undefined}> {
+export default class Home extends React.Component<HomeProps, { data: Map | undefined }> {
     private podManager = new PODManager();
     private webID: string;
     private maps: Array<Map>;
     private data: Map;
     static defaultProps = {
-        placeList : []
+        placeList: []
     }
 
-    public constructor(props : any) {
+    public constructor(props: any) {
         super(props);
         this.webID = SolidSessionManager.getManager().getWebID();
         this.maps = new Array();
@@ -31,8 +30,8 @@ export default class Home extends React.Component<HomeProps,{data:Map|undefined}
         };
 
         let map = new Map("Public map");
-        let places : Array<PlaceType> = props.placeList;
-        let placemarks : Array<Placemark> = places.map((place) => {
+        let places: Array<PlaceType> = props.placeList;
+        let placemarks: Array<Placemark> = places.map((place) => {
             return new Placemark(place.latitude, place.longitude, place.title);
         })
         placemarks.forEach((placemark) => map.add(placemark));
@@ -46,16 +45,17 @@ export default class Home extends React.Component<HomeProps,{data:Map|undefined}
 
     public async componentDidMount(): Promise<void> {
         this.maps = await this.podManager.getAllMaps();
-        let map = (this.maps.length > 0) ? this.maps[0] : new Map('TestMap');;
+        let map = (this.maps.length > 0) ? this.maps[0] : new Map('TestMap');
+        ;
         await this.podManager.loadPlacemarks(map);
         this.setState({data: map});
     }
 
     private async changeMap(event: ChangeEvent): Promise<void> {
-        let select:HTMLSelectElement = (event.target as HTMLSelectElement);
-        let index:number = select.selectedIndex;
-        let id:string = select.options[index].value;
-        let map:Map|undefined = this.maps.find(m => m.getId() == id);
+        let select: HTMLSelectElement = (event.target as HTMLSelectElement);
+        let index: number = select.selectedIndex;
+        let id: string = select.options[index].value;
+        let map: Map | undefined = this.maps.find(m => m.getId() == id);
 
         if (map !== undefined) {
             this.setState({data: undefined});
@@ -67,24 +67,32 @@ export default class Home extends React.Component<HomeProps,{data:Map|undefined}
     public render(): JSX.Element {
         return (
             <section className='Home'>
-                <button onClick={async () => {await this.podManager.saveMap(new Map("Initial map"))}}>Save</button>
+                <button onClick={async () => {
+                    await this.podManager.saveMap(new Map("Initial map"))
+                }}>Save
+                </button>
                 <h2>{this.state.data?.getName() || "Loading"}</h2>
                 {this.state.data !== undefined &&
-                <div>
-                    <select name="map" onChange={this.changeMap.bind(this)}>
-                        {this.maps.map(m => {
-                            return (<option value={m.getId()}>{m.getName()}</option>)
-                        })}
-                    </select>
-                    <div className = "content">
-                        <MapFilter></MapFilter>
-                        <div className="map">
-                        <LeafletMapAdapter map={this.state.data}/>
+                    <div>
+                        <select name="map" onChange={this.changeMap.bind(this)}>
+                            {this.maps.map(m => {
+                                return (<option value={m.getId()}>{m.getName()}</option>)
+                            })}
+                        </select>
+                        <div className="content">
+                            <MapFilter></MapFilter>
+                            <div className="map">
+                                <LeafletMapAdapter map={this.state.data}/>
+                            </div>
                         </div>
                     </div>
-                </div>}
-                <input type="button" value="set public" onClick={async () => {await this.podManager.setPublicAccess(this.podManager.getBaseUrl()+"/data/maps/"+this.maps[0].getId(), true)}} />
-                <input type="button" value="set private" onClick={async () => {await this.podManager.setPublicAccess(this.podManager.getBaseUrl()+"/data/maps/"+this.maps[0].getId(), false)}} />
+                }
+                <input type="button" value="set public" onClick={async () => {
+                    await this.podManager.setPublicAccess(this.podManager.getBaseUrl() + "/data/maps/" + this.maps[0].getId(), true)
+                }}/>
+                <input type="button" value="set private" onClick={async () => {
+                    await this.podManager.setPublicAccess(this.podManager.getBaseUrl() + "/data/maps/" + this.maps[0].getId(), false)
+                }}/>
             </section>
         );
     }
