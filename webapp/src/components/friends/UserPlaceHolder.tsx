2dd6b11daa1a8ff41f9ed0c19614a40639f40f53
Pelayo-Reguera <UO282348@uniovi.es>
2023-04-20 15:50:36 +0200

    css

---
+++ webapp/src/components/friends/UserPlaceHolder.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+import User from "../../domain/User";
+import {Avatar, Box, Card, CardActionArea, CardContent, CardHeader, Typography} from "@mui/material";
+import UserPage from "./UserPage";
+
+interface UserPlaceState {
+    changePage: boolean
+}
+
+interface UserPlaceHolderProps {
+    user: User
+    callback: (component: JSX.Element) => void
+}
+
+/**
+ * A placeholder to preview the user, if clicked it shows the full UserPage of the user.
+ * @param {User} user - The user to be previewed
+ * @param {(component:JSX.Element)=>void} callback - a callback function to present the UserPage over
+ *                                                   the Friends element.
+ * @author UO283069
+ */
+export default class UserPlaceHolder extends React.Component<UserPlaceHolderProps, UserPlaceState> {
+
+    /**
+     * The user that is being represented
+     * @readonly
+     * @private
+     */
+    private readonly user: User;
+
+    constructor(props: UserPlaceHolderProps) {
+        super(props);
+        this.user = props.user;
+        this.state = {
+            changePage: false
+        }
+    }
+
+    private getFriendInfo(user: User) {
+        this.props.callback(<UserPage user={user}></UserPage>);
+        this.setState({
+            changePage: true
+        });
+    }
+
+    render() {
+        return <Box style={{
+            display: "flex",
+            flexWrap: "wrap",
+        }}>
+            <Card className="card">
+                <CardActionArea sx={{height: "100%"}} className="card" onClick={() => {
+                    this.getFriendInfo(this.user)
+                }}>
+                    <CardHeader avatar={<Avatar alt="User avatar"
+                                                sx={{
+                                                    backgroundColor: "#B2CCEB",
+                                                    width: 60,
+                                                    height: 60
+                                                }}>{this.user.getName()?.charAt(0)}</Avatar>}
+                    />
+                    <CardContent>
+                        <h3>{this.user.getName()}</h3>
+                        <Typography>Friend text</Typography>
+                        <a href={this.user.getWebId()}>SOLID profile</a>
+                    </CardContent>
+                </CardActionArea>
+            </Card>
+        </Box>
+            ;
+    }
+
+}
