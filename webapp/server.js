9a0c8b3d7f9ef6729536558ef976d08f321733d1
Iván Menéndez Mosegui <98897888+uo282892@users.noreply.github.com>
2023-04-26 23:42:19 +0200

    Changed structure of Dockerfile and server.js

---
+++ webapp/server.js
@@ -0,0 +1,35 @@
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
+const express = require('express');
+var expressStaticGzip = require('express-static-gzip');
+const path = require('path');
+
+//Load certificates
+var privateKey  = fs.readFileSync('claves/privkey.pem');
+var certificate = fs.readFileSync('claves/fullchain.pem');
+var credentials = {key: privateKey, cert: certificate};
+
+var app = express();
+
+//This will make sure that we will serve everything through https
+app.all('*', function(req, res, next){
+    if (req.secure) {
+        return next();
+    }
+    res.redirect('https://'+req.hostname + req.url);
+});
+
+//Base path of our application. We serve first the brotli version (compression).
+app.use('/', expressStaticGzip('build', {
+    enableBrotli: true,
+    orderPreference: ['br']
+}));
+
+//For react routes to work, fallback to index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+});
+
+var httpsServer = https.createServer(credentials, app);
+httpsServer.listen(443);
