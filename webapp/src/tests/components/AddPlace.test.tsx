7389410921588642b79581dab99b0ac31598d34f
Carlos Triana <uo283428@uniovi.es>
2023-05-01 19:56:45 +0200

    AddPlace test

--- webapp/src/tests/components/AddPlace.test.tsx
+++ webapp/src/tests/components/AddPlace.test.tsx
@@ -5,20 +5,29 @@ import SolidSessionManager from '../../adapters/solid/SolidSessionManager';
 import FriendManager from '../../adapters/solid/FriendManager';
 import User from '../../domain/User';
 import AddPlace from '../../components/place/AddPlace';
+import userEvent from '@testing-library/user-event';
 
 const latitude = 0.2;
 const longitude = 0.9;
 const title = "Test";
 const category = "Test";
 
-beforeAll(() => {
+const crypto = require('crypto');
+
+Object.defineProperty(globalThis, 'crypto', {
+    value: {
+        randomUUID: () => crypto.randomUUID()
+    }
+});
+
+beforeEach(() => {
     // As the session manager uses fetching functions
     jest.spyOn(SolidSessionManager.prototype, "isLoggedIn").mockImplementation(() => {
         return true;
-    })
+    });
     jest.spyOn(SolidSessionManager.prototype, "getWebID").mockImplementation(() => {
         return "https://testID";
-    })
+    });
     jest.spyOn(FriendManager.prototype, "getFriendsList").mockImplementation(() => {
         let friendsList = new Array<User>();
         const user1 = new User("testFriend1", "https://testFriend1");
@@ -27,7 +36,7 @@ beforeAll(() => {
         friendsList.push(user2);
         return Promise.resolve(friendsList);
     });
-})
+});
 
 test('The AddPlace component is rendering correctly', async () => {
     const {getByText} = render(<AddPlace open={true} placemark={new Placemark(latitude, longitude)} />)
@@ -38,7 +47,7 @@ test('The AddPlace component is rendering correctly', async () => {
         expect(getByText("Description:")).toBeInTheDocument();
         expect(getByText("Photo:")).toBeInTheDocument();
         expect(getByText("Submit")).toBeInTheDocument();
-    })
+    });
 });
 
 /*
@@ -61,4 +70,78 @@ test('The AddPlace component is working as expected', async () => {
         fireEvent.change(descriptionTextarea, { target: { value: 'Test' } });
         expect(descriptionTextarea.value).toBe('Test');
     })
+});
+
+test('User can add images to the place', async () => {
+    Object.defineProperty(URL, 'createObjectURL', {
+        value: jest.fn(() => 'mock-url'),
+      });
+      Object.defineProperty(URL, 'revokeObjectURL', {
+        value: jest.fn(() => 'mock-url'),
+      });
+    const { getByPlaceholderText } = render(<AddPlace open={true} placemark={new Placemark(latitude, longitude)} />)
+    await waitFor(() => {
+        const photoInput = getByPlaceholderText('Choose a photo') as HTMLInputElement;
+        fireEvent.change(photoInput, { target: { files: [new File(['test'], 'test.jpg', { type: 'image/jpeg' })] } });
+        expect(photoInput.files?.length).toBe(1);
+    })
+});
+
+/**
+ * JEST test for testing if the category of the place can be changed.
+ * First, the select tag of name "category".
+ */
+test('User can change the category of the place', async () => {
+    const { getByPlaceholderText } = render(<AddPlace open={true} placemark={new Placemark(latitude, longitude)} />)
+    await waitFor(() => {
+        const categorySelect = screen.getByTitle('category') as HTMLSelectElement;
+        userEvent.selectOptions(categorySelect, ['museum']); // Replace 'some-value' with an actual value from your categories
+        fireEvent.change(categorySelect, { target: { value: 'museum' } });
+        expect(categorySelect.value).toBe('museum');
+    });
+});
+
+test('input fields are rendered and can be updated', () => {
+    const {getByText} = render(<AddPlace open={true} placemark={new Placemark(latitude, longitude)} />)
+
+    const nameInput = screen.getByPlaceholderText('Name') as HTMLInputElement;
+
+    const descriptionTextarea = screen.getByPlaceholderText('Introduce a description') as HTMLTextAreaElement;
+  
+    fireEvent.change(nameInput, { target: { value: 'Test Place' } });
+    fireEvent.change(descriptionTextarea, { target: { value: 'Test Desc' } });
+  
+    expect(nameInput).toHaveValue('Test Place');
+    expect(descriptionTextarea).toHaveValue('Test Desc');
+});
+
+test('form can be submitted and proper action is executed', async () => {
+    const mockOnSubmit = jest.fn();
+    const { getByPlaceholderText, getByRole } = render(
+        <AddPlace
+            open={true}
+            placemark={new Placemark(latitude, longitude)}
+            callback={mockOnSubmit}
+        />
+    );
+
+    const nameInput = getByPlaceholderText('Name') as HTMLInputElement;
+    const descriptionTextarea = getByPlaceholderText(
+        'Introduce a description'
+    ) as HTMLTextAreaElement;
+    const submitButton = getByRole('button', { name: 'Submit' });
+
+    fireEvent.change(nameInput, { target: { value: 'Test Place' } });
+    fireEvent.change(descriptionTextarea, { target: { value: 'Test Desc' } });
+    fireEvent.click(submitButton);
+
+    // Wait for the callback to be called once
+    await waitFor(() => expect(mockOnSubmit).toHaveBeenCalledTimes(1));
+
+    // Get the first argument passed to the mock function during the first call
+    const submittedPlacemark = mockOnSubmit.mock.calls[0][0];
+
+    // Perform custom assertions on specific fields
+    expect(submittedPlacemark.title).toBe('Test Place');
+    expect(submittedPlacemark.category).toBe('restaurant');
 });
