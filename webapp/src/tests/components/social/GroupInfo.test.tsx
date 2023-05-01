5f1eaf440654d7a570292f34a7a59e4e8f068be6
Carlos Triana <uo283428@uniovi.es>
2023-05-01 16:07:46 +0200

    Working on GroupInfo

--- webapp/src/tests/components/social/GroupInfo.test.tsx
+++ webapp/src/tests/components/social/GroupInfo.test.tsx
@@ -2,16 +2,25 @@ import React from 'react';
 import {fireEvent, getByPlaceholderText, render, waitFor, screen} from '@testing-library/react';
 import UserPage from '../../../components/social/UserPage';
 import User from '../../../domain/User';
+import GroupInfo from '../../../components/social/GroupInfo';
+import Group from '../../../domain/Group';
 
-const name = "test1";
-const webID = "testWebID";
+const name = "test";
+const name1 = "test1";
+const webID1 = "testWebID1";
+const name2 = "test2";
+const webID2 = "testWebID2";
+const testUser1 = new User(name1, webID1);
+const testUser2 = new User(name2, webID2);
+const members = [testUser1, testUser2];
+
 
 beforeAll(() => {
     // As the session manager uses fetching functions
 })
 
 test('The UserPage component is rendering correctly', async () => {
-    const {getByText} = render(<UserPage user={new User(name, webID)} />)
+    const {getByText} = render(<GroupInfo group={new Group(name, members)} />)
     await waitFor(() => {
         expect(getByText("test1")).toBeInTheDocument();
         expect(getByText("Friends places")).toBeInTheDocument();
