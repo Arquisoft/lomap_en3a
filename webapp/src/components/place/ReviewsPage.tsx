ee7b24c4f59e903a876e2e447cba881e52963bdf
Carlos Triana <uo283428@uniovi.es>
2023-05-02 20:34:51 +0200

    Review loading

--- webapp/src/components/place/ReviewsPage.tsx
+++ webapp/src/components/place/ReviewsPage.tsx
@@ -10,6 +10,7 @@ interface ReviewsPageState {
     comments: PlaceComment[];
     score: number;
     loading: boolean;
+    loadingRating: boolean;
 }
 
 export default class ReviewsPage extends React.Component<IPlacePageProps, ReviewsPageState> {
@@ -25,22 +26,32 @@ export default class ReviewsPage extends React.Component<IPlacePageProps, Review
         this.state = {
             comments: [],
             loading: true,
-            score: 0
+            score: 0,
+            loadingRating: true,
         }
     }
 
     public async componentDidMount(): Promise<void> {
-        this.setState({comments: [], loading: true})
+        this.getComments((comments) => this.setState({comments: comments, loading: false}));
+        this.getScore((score) => this.setState({score: score, loadingRating: false}));
+    }
+
+    public async getComments(callback: (comments: any) => void) {
         let comments = await this.pod.getComments(this.props.placeUrl as string);
+        callback(comments);
+    }
+
+    public async getScore(callback: (rating: any) => void) {
         let score = (await this.pod.getScore(this.props.placeUrl as string)).score;
-        this.setState({comments: comments, score: score, loading: false})
+        callback(score);
     }
 
     render() {
         return <div className="ReviewsPage">
             <h3>Average Rating</h3>
             <div>
-                <Rating readonly initialValue={this.state.score}></Rating>
+            {this.state.loadingRating && <LoadingPage style={{left: "20%", padding: "1em"}} size={50}/>}
+            {!this.state.loadingRating && <Rating readonly initialValue={this.state.score}></Rating>}
             </div>
             <h3>Comments</h3>
             {this.state.loading && <LoadingPage style={{left: "20%", padding: "1em"}} size={50}/>}
