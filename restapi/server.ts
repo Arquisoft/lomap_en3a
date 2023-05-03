436931f11d1100e9ef943bf3d509ee821dd3d270
Pelayo-Reguera <UO282348@uniovi.es>
2023-05-03 23:26:41 +0200

    Configuration to deploy the restAPI with HTTPS

--- restapi/server.ts
+++ restapi/server.ts
@@ -2,11 +2,26 @@ import express, { Application, RequestHandler } from "express";
 import cors from 'cors';
 import bp from 'body-parser';
 import promBundle from 'express-prom-bundle';
+import fs from "fs";
+import https from "https";
 import api from "./api";
 import db_uri from "./settings";
 
 const app: Application = express();
 const port: number = 5000;
+
+//Load certificates
+var privateKey  = fs.readFileSync('claves/alice.key');
+var certificate = fs.readFileSync('claves/alice.crt');
+var credentials = {key: privateKey, cert: certificate};
+
+//This will make sure that we will serve everything through https
+app.all('*', function(req, res, next){
+    if (req.secure) {
+        return next();
+    }
+    res.redirect('https://'+req.hostname + ":" + port + req.url);
+});
 
 const mongoose = require("mongoose")
 const uri: string = db_uri;
@@ -23,9 +38,17 @@ app.use(bp.json());
 
 app.use("/api", api)
 
-app.listen(port, ():void => {
-    console.log('Restapi listening on '+ port);
-}).on("error",(error:Error)=>{
-    console.error('Error occured: ' + error.message);
-});
+// app.listen(port, ():void => {
+//     console.log('Restapi listening on '+ port);
+// }).on("error",(error:Error)=>{
+//     console.error('Error occured: ' + error.message);
+// });
 
+var httpsServer = https.createServer(credentials, app);
+httpsServer.listen(port)
+// httpsServer.listen(port, (): void => {
+//     console.log("Restapi listening on " + port);
+// })
+//     .on("error", (error: Error) => {
+//         console.error("Error occured: " + error.message);
+//     });
