f241dd3abf1314b346636259ac3751836be3edd3
Iván Menéndez Mosegui <98897888+uo282892@users.noreply.github.com>
2023-05-04 04:50:15 +0200

    final commit, e2e working

--- webapp/e2e/steps/filter.steps.ts
+++ webapp/e2e/steps/filter.steps.ts
@@ -50,7 +50,7 @@ defineFeature(feature, test => {
 
       await page.mouse.move(bounding_box!.x + bounding_box!.width / 2, bounding_box!.y + bounding_box!.height / 2);
       await page.mouse.down();
-      await page.mouse.move(Math.floor(Math.random() * 25), Math.floor(Math.random() * 25));
+      await page.mouse.move(Math.floor(Math.random() * 100), Math.floor(Math.random() * 100));
       await page.mouse.up()
       await page.waitForTimeout(2000)
       await expect(page).toClick('.content > :nth-child(1) > :nth-child(1)');
@@ -60,7 +60,7 @@ defineFeature(feature, test => {
         name: placeName,
         description: "This is a test place"
       });
-      await expect(page).toSelect('select[name="category"]', "park");
+      await expect(page).toSelect('select[name="category"]', "Park");
       await expect(page).toClick('button', {text: "Submit"});
     });
 
@@ -85,6 +85,7 @@ defineFeature(feature, test => {
     let username:string;
     let password:string;
     let placeName:string = "Test" + Math.floor(Math.random() * 100000);
+    let numberElements = 0;
 
     given('A user that has a park in the map', async () => {
       username = "testlomapen3a"
@@ -107,7 +108,7 @@ defineFeature(feature, test => {
 
       await page.mouse.move(bounding_box!.x + bounding_box!.width / 2, bounding_box!.y + bounding_box!.height / 2);
       await page.mouse.down();
-      await page.mouse.move(Math.floor(Math.random() * 50) + 50, Math.floor(Math.random() * 50) + 50);
+      await page.mouse.move(Math.floor(Math.random() * 50) + 150, Math.floor(Math.random() * 50) + 150);
       await page.mouse.up()
       await page.waitForTimeout(2000)
       await expect(page).toClick('.content > :nth-child(1) > :nth-child(1)');
@@ -117,15 +118,16 @@ defineFeature(feature, test => {
         name: placeName,
         description: "This is a test place"
       });
-      await expect(page).toSelect('select[name="category"]', "park");
+      await expect(page).toSelect('select[name="category"]', "Park");
       await expect(page).toClick('button', {text: "Submit"});
     });
 
 
     when('The users selects the museum category in the filter and clicks Search', async () => {
+      numberElements = (await page.$$('img.leaflet-marker-icon')).length
       const [showFiltersButton] = await page.$x('//*[@id="basic-button"]')
       await showFiltersButton.click();
-      await expect(page).toClick('input[value="museum"]')
+      await expect(page).toClick('input[value="Museum"]')
       await page.waitForTimeout(5000)
       await expect(page).toClick('.search-filter')
     });
@@ -133,7 +135,7 @@ defineFeature(feature, test => {
     then('The museum can not be seen in the map', async () => {
       await expect(page).toClick('.MuiBackdrop-root')
       //There are no places in the map
-      await expect(page).not.toMatchElement('img.leaflet-marker-icon:last-child')
+      await expect(page).not.toMatchElement('img.leaflet-marker-icon:nth-child(' + numberElements + ')')
     });
   })
 
