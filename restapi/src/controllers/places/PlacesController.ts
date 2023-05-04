9df9e76d0088eb9b3c0b6f14f0c4e390995a91b9
Iván Menéndez Mosegui <98897888+uo282892@users.noreply.github.com>
2023-05-04 04:05:46 +0200

    Fixed confilcts in PlacesController

--- restapi/src/controllers/places/PlacesController.ts
+++ restapi/src/controllers/places/PlacesController.ts
@@ -7,11 +7,17 @@ import { Document } from "mongoose";
 const getPlaces = async (req: Request, res: Response): Promise<Response> => {
     try {
         res.header('Access-Control-Allow-Origin', '*');
-        const places: Array<PlaceType> = await Promise.resolve(Place.find());
+        const documents: Array<Document<unknown, {}, Placemark>> = await Promise.resolve(
+            Place.find()
+        );
+        const places: Array<Placemark> = documents.map((doc) =>
+            doc.toObject() as Placemark
+        );
         return res.status(200).send(places);
-    }
-    catch (error) {
-        return res.status(500).send("An error has occurred while retrieving the list of places: \n\n" + error)
+    } catch (error) {
+        return res
+            .status(500)
+            .send("An error has occurred while retrieving the list of places: \n\n" + error);
     }
 }
 
@@ -31,28 +37,6 @@ const addPlace = async (req: Request, res: Response): Promise<Response> => {
         .send("An error has occurred while retrieving the list of places: \n\n" + error);
     }
 };
-
-const addPlace = async (req: Request, res: Response): Promise<Response> => {
-    try {
-        res.header('Access-Control-Allow-Origin', '*');
-        console.log(req.body);
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({errors: errors.array()})
-        }
-        await Place.create({
-            longitude: req.body.longitude.toString(),
-            latitude: req.body.latitude.toString(),
-            title: req.body.title.toString(),
-            placeUrl: req.body.placeUrl.toString(),
-            category: req.body.category.toString()
-        });
-        return res.sendStatus(200)
-    }
-    catch (error) {
-        return res.status(500).send("An error has occurred while adding a place: " + error)
-    }
-}
 
 const deletePlace = async (req: Request, res: Response): Promise<Response> => {
     try {
