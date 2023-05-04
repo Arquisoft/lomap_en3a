d9f9c65a02cb317cfda3d7cb128aeaa8f6689ab0
David Martínez Castañón <UO282048@uniovi.es>
2023-05-04 03:50:09 +0200

    test(MapView): component renders

---
+++ webapp/src/tests/components/MapView.test.tsx
@@ -0,0 +1,24 @@
+import {PlaceCategory} from "../../domain/place/PlaceCategory";
+import {act, fireEvent, getByText, render, screen, waitFor} from "@testing-library/react";
+import MapFilter from "../../components/MapFilter";
+import React from "react";
+import PODManager from "../../adapters/solid/PODManager";
+import Map from "../../domain/Map";
+import Placemark from "../../domain/Placemark";
+import MapView from "../../pages/MapView";
+
+beforeAll(() => {
+    jest.spyOn(PODManager.prototype, "getAllMaps").mockImplementation(async (user: string | undefined) => {
+        return [new Map("myMap")];
+    })
+    jest.spyOn(PODManager.prototype, "loadPlacemarks").mockImplementation(async (map: Map, author: string | undefined) => {
+        map.setPlacemarks([new Placemark(0,0)]);
+    })
+});
+
+test('The page renders correctly', async () => {
+    render(<MapView/>)
+    await waitFor(() => {
+        expect(document.querySelector(".map-header")).not.toBeNull();
+    });
+});
